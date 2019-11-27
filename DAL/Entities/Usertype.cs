72094af471c33422320d5971efb8204c9386750d
Moid Shams <me_moid@hotmail.com>
2019-11-27 21:22:08 +0500

    Modified User.cs Added Usertype.cs

--- DAL/Entities/Usertype.cs
+++ DAL/Entities/Usertype.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace DAL.Entities
 {
-    class User
+    class Usertype
     {
         public int Id { get; set; }
-        public string name { get; set; }
     }
 }
