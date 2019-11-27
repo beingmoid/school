c43c32128b9e5a2eee9bf190057060fcf8f1fdfe
Moid Shams <me_moid@hotmail.com>
2019-11-27 21:49:47 +0500

    Added BaseEntity.cs

---
+++ DAL/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class BaseEntity
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public DateTime? CreatedTime  { get; set; }
+        public DateTime EditedTime { get; set; }
+        public int CreatedBy { get; set; }
+        public int EditedBy { get; set; }
+    }
+}
